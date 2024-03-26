6f863acbab1e567984dc88e5291086a7a4293a44
Nam Nguyen <nguy1053@msu.edu>
2024-03-26 11:22:25 -0400

    HomePage: Title and Intro/About

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './HomePage.css'
 
 const HomePage = () => {
   return (
     <div>
-        About PeacePod  
+        {/* App name */}
+        <div className='app-name__container'>
+          <p className='app-name__text'>PeacePod</p>
+          <div className='app-intro__container'>
+            <h1>PeacePod: bridging gaps, healing minds</h1>
+            <p className='app-intro__description'>A website designed to foster connections between individuals from similar 
+            backgrounds in mental state for supportive and meaningful conversations. 
+            We understand the importance of finding sympathy and empathy in others 
+            who share similar experiences, which is why this app is here to bridge the gap 
+            and provide a platform for genuine connection and support.</p>
+          </div>
+        </div>
     </div>
   )
 }
