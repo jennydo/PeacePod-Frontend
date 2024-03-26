0f28dbdda7a5b54df28f3ee9df9336b5d6abc483
Hennessy Nguyen <anguyen137@fordham.edu>
2024-03-26 17:28:52 -0400

    create the route to each page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+import Feed from './Feed';
+import Messages from './Messages';
+import Meditation from './Meditation';
+import Profile from './Profile';
+import NotFound from './NotFound';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+         <Navbar />
+          <div className="content">
+            <Switch>
+              <Route exact path = "/">
+                <Home />
+              </Route>
+              <Route path = "/feed">
+                <Feed />
+              </Route>
+              <Route path="/messages">
+                <Messages />
+              </Route>
+              <Route path="/meditation">
+                <Meditation />
+              </Route>
+              <Route path="/profile">
+                <Profile />
+              </Route>
+              <Route path = "*">
+                <NotFound/>
+             </Route>
+           </Switch>
+         </div>
+       </div>
+    </Router>
   );
 }
 
