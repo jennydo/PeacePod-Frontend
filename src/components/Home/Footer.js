f5652c9598091c4b3b8b7fbc34d8ff67b08277ec
Hennessy Nguyen <anguyen137@fordham.edu>
2024-04-05 19:23:23 -0400

    change the cloud

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRss, faComments, faHeadphones } from '@fortawesome/free-solid-svg-icons';
 import "./Footer.css"
-import { Text, Stack, VStack } from "@chakra-ui/react";
+import { Text, VStack } from "@chakra-ui/react";
 
 const Cards = [
     {
@@ -25,19 +25,30 @@ const Cards = [
 
 export const ButtonCards = () => {
     return (
-        <Stack className="button-container" direction='row' spacing={20} align='center'>
-        {Cards.map((card, index) => (
-            <Link to={card.to}>
-                <Stack className="button-card" direction='column' spacing={10} align="center">
-                    <div id="cloud">
-                        <VStack className="button-content" direction='column' spacing={8} w={300} align="center">
-                            {card.icon && <FontAwesomeIcon icon={card.icon} className="button-icon" />}
-                            <Text className="button-label">{card.label}</Text>
-                        </VStack>
-                    </div>
-                </Stack>
-            </Link>
-        ))}
-        </Stack>
+        <div className="button-container" direction='row' align='center'>
+            {Cards.map((card, index) => (
+                <div className="button-card" key={index}>
+                    <Link to={card.to}>
+                        <div className="button">
+                            <div className="cloud">
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
+                                    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round">
+                                        </g>
+                                        <g id="SVGRepo_iconCarrier"> 
+                                            <path d="M16.2857 20C19.4416 20 22 17.4717 22 14.3529C22 11.8811 20.393 9.78024 18.1551 9.01498C17.8371 6.19371 15.4159 4 12.4762 4C9.32028 4 6.7619 6.52827 6.7619 9.64706C6.7619 10.3369 6.88706 10.9978 7.11616 11.6089C6.8475 11.5567 6.56983 11.5294 6.28571 11.5294C3.91878 11.5294 2 13.4256 2 15.7647C2 18.1038 3.91878 20 6.28571 20H16.2857Z" fill="#ffffff">
+                                            </path> 
+                                    </g>
+                                </svg>
+                            </div>
+                            <VStack className="button-content" direction='column' spacing={8} w={300} align="center">
+                                {card.icon && <FontAwesomeIcon icon={card.icon} className="button-icon" />}
+                                <Text className="button-label">{card.label}</Text>
+                            </VStack>
+                        </div>
+                    </Link>
+                </div>
+            ))}
+        </div>
     );
 };
