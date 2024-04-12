bd5adaa32e5f05e34fefc4f0266c8880b1c9e149
Jenny <dolientrang@gmail.com>
2024-04-12 18:20:37 -0400

    quotes card

--- src/pages/NewsFeed.js
+++ src/pages/NewsFeed.js
@@ -1,16 +1,25 @@
 import CreatePost from "../components/NewsFeed/CreatePost";
 import AllPosts from "../components/NewsFeed/AllPosts";
 import PromptPost from "../components/NewsFeed/PromptPost";
+import QuotesAndTipsCards from "../components/NewsFeed/QuotesAndTipsCards";
+import {Grid, GridItem } from "@chakra-ui/react"
 
 const NewsFeed = () => {
-    return ( 
-        <div>
-            <h1>NewsFeed</h1>
-            <CreatePost/>
+    return (
+      <div>
+        <h1>NewsFeed</h1>
+        <Grid templateColumns="repeat(1, 2fr 1fr)">
+          <GridItem>
+            <CreatePost />
             <PromptPost />
-            <AllPosts/>
-        </div>
-     );
+            <AllPosts />
+          </GridItem>
+          <GridItem>
+            <QuotesAndTipsCards />
+          </GridItem>
+        </Grid>
+      </div>
+    );
 }
  
 export default NewsFeed;
