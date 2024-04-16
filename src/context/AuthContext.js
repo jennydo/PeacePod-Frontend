e8b623c2e0e9038dbd50c9c60de931ddffb9fa7d
Hennessy Nguyen <anguyen137@fordham.edu>
2024-04-16 11:16:50 -0400

    create AuthContext and useAuthContext

---
+++ src/context/AuthContext.js
@@ -0,0 +1,22 @@
+import { createContext, useReducer } from 'react';
+
+export const AuthContext = createContext();
+
+export const authReducer = (state, action) => {
+    switch (action.type) {
+        case 'LOGIN':
+            return { user: action.payload}
+    }
+}
+
+export const AuthContextProvider = ( {children} ) => {
+    const [state, dispatch] = useReducer(authReducer, {
+        user: null
+    })
+    console.log("AuthContext state: ", state)
+    return (
+        <AuthContext.Provider value={{...state, dispatch}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
