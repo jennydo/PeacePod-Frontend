ed31dca39823dc124e174a78e7f1ed30ff8f4562
Hennessy Nguyen <anguyen137@fordham.edu>
2024-04-16 14:06:25 -0400

    Add Redirection for user when log in and log out

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ import Chat from './pages/Chat';
 import Meditation from './pages/Meditation';
 import UserProfile from './pages/UserProfile';
 import NotFound from './pages/NotFound';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import SignUpPage from './pages/SignUpPage';
 import LogInPage from './pages/LogInPage';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { user } = useAuthContext()
+
   return (
     <ChakraProvider className="">
       <div className="">
@@ -20,12 +23,12 @@ function App() {
           <Navbar />
           <Routes>
             <Route exact path="/" element={<HomePage />} />
-            <Route path="/signup" element={<SignUpPage/>} />
-            <Route path="/login" element={<LogInPage/>} />
-            <Route path="/newsfeed" element={<NewsFeed />} />
-            <Route path="/chat" element={<Chat />} />
-            <Route path="/meditation" element={<Meditation />} />
-            <Route path="/userprofile" element={<UserProfile />} />
+            <Route path="/signup" element={(!user) ? <SignUpPage/> : <Navigate to="/"/>} />
+            <Route path="/login" element={(!user) ? <LogInPage/> : <Navigate to="/"/>} />
+            <Route path="/newsfeed" element={user ? <NewsFeed /> : <Navigate to="/login"/>} />
+            <Route path="/chat" element={user ? <Chat /> : <Navigate to="/login"/>} />
+            <Route path="/meditation" element={user ? <Meditation /> : <Navigate to="/login"/>} />
+            <Route path="/userprofile" element={user ? <UserProfile /> : <Navigate to="/login"/>} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Router>
