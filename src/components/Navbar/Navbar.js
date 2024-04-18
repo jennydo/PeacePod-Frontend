04b077ba200be9045fd437a1210857a468a93b7b
Hennessy Nguyen <anguyen137@fordham.edu>
2024-04-18 14:30:30 -0400

    Make the logout button prettier

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,23 @@
 import {Link, useLocation} from 'react-router-dom';
 import { useLogOut } from '../../hooks/useLogOut';
-import { useAuthContext } from '../../hooks/useAuthContext';
+import { useAuthContext } from '../../hooks/useAuthContext'
 import './Navbar.css'
+import { Avatar, Menu, MenuButton, MenuItem, MenuList, MenuDivider, 
+    useDisclosure, Button, Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton, } from '@chakra-ui/react';
 
 const Navbar = () => {
     const {logOut} = useLogOut();
-    const {user} = useAuthContext();
+    const { user } = useAuthContext()?.user || {};
+    const { username, avatar } = user || {};
     const location = useLocation();
     const pathname = location.pathname;
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
     return ( 
         <nav className="peacepod-navbar">
@@ -19,8 +29,42 @@ const Navbar = () => {
                         <Link to="/newsfeed" className={`peacepod-navlink ${pathname === "/newsfeed" ? "active" : ""}`}>NewsFeed</Link>
                         <Link to="/chat" className={`peacepod-navlink ${pathname === "/chat" ? "active" : ""}`}>Messages</Link>
                         <Link to="/meditation" className={`peacepod-navlink ${pathname === "/meditation" ? "active" : ""}`}>Meditation</Link>
-                        <Link to="/userprofile" className={`peacepod-navlink ${pathname === "/userprofile" ? "active" : ""}`}>Profile</Link>
-                        <button onClick={() => logOut()}>Logout</button>
+                        <Menu>
+                            <MenuButton px={5} mb={100}>
+                                <Avatar name={username} src={avatar} size='md'/>
+                            </MenuButton>
+                            <MenuList>
+                                <MenuItem>
+                                    <Link to="/userprofile" className={`peacepod-navlink ${pathname === "/userprofile" ? "active" : ""}`}>
+                                        My Profile
+                                    </Link>
+                                </MenuItem>
+                                <MenuDivider />
+                                <MenuItem>
+                                    <button onClick={onOpen}>
+                                            Logout
+                                    </button>
+                                </MenuItem>
+                            </MenuList>
+                        </Menu>
+
+                        <Modal isOpen={isOpen} onClose={onClose}>
+                            <ModalOverlay />
+                            <ModalContent>
+                            <ModalHeader>Logout</ModalHeader>
+                            <ModalCloseButton />
+                            <ModalBody>
+                                Are you sure you want to log out of your account?
+                            </ModalBody>
+
+                            <ModalFooter>
+                                <Button variant='ghost' onClick={onClose} mr={3}>Cancel</Button>
+                                <Button colorScheme='blue' mr={3} onClick={() => logOut()} alignContent={"center"}>
+                                Logout
+                                </Button>
+                            </ModalFooter>
+                            </ModalContent>
+                        </Modal>
                     </div>
                 )}   
                 {(!user) && (
