c3c969eb7cd56a3697a8859f6a94e00027a77ff9
Hennessy Nguyen <anguyen137@fordham.edu>
2024-04-21 12:49:38 -0400

    create chat context"

---
+++ src/hooks/useChatsContext.js
@@ -0,0 +1,10 @@
+import { ChatsContext } from "../context/ChatsContext";
+import { useContext } from "react";
+
+export const useChatsContext = () => {
+    const context = useContext(ChatsContext);
+    if (!context) {
+        throw Error("useChatsContext must be used inside a ChatsContextProvider")
+    }
+    return context;
+}
