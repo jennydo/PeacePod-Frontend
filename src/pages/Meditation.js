8ebef6dec58efc8b44d8cfde86541b191096335a
Hennessy Nguyen <anguyen137@fordham.edu>
2024-05-07 14:07:00 -0400

    edit page Meditation to add Grid and the Player

--- src/pages/Meditation.js
+++ src/pages/Meditation.js
@@ -1,15 +1,20 @@
-import SpotifyList from '../components/Meditation/Music/SpotifyList';
-import SpotifyLogin from '../components/Meditation/Music/SpotifyLogin';
 import { useSpotifyContext } from '../hooks/useSpotifyContext'
+import MeditationDrawer from '../components/Meditation/MeditationDrawer';
+import SongPlayer from '../components/Meditation/Music/SongPlayer';
+import { Grid, GridItem } from '@chakra-ui/react';
 
 const Meditation = () => {
-    const {spotifyCode} = useSpotifyContext();
+    const {accessToken, playingTrack} = useSpotifyContext();
 
-    return ( 
-        <>
-            {spotifyCode? <SpotifyList code={spotifyCode}/> : <SpotifyLogin/>}
-        </>
-
+    return (  
+        <Grid gridTemplateRows={'1fr 10%'} height="85vh" gap={6}>
+            <GridItem w='100%' h='100%' bg='blue.500'>
+                <MeditationDrawer/>
+            </GridItem>
+            <GridItem w='100%' h='100%'>
+                {playingTrack && <SongPlayer accessToken={accessToken} trackUri={playingTrack?.uri}/>}
+            </GridItem>
+        </Grid>
      );
 }
  
