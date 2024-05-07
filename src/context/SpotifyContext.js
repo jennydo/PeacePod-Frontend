383d7a5c6f20916cb7cc4a43377b80ba099c9772
Hennessy Nguyen <anguyen137@fordham.edu>
2024-05-07 14:11:22 -0400

    SpotifyContext: save to localStorage when spotifyCode is updated. & add action.type SET_PLAYING_TRACK

--- src/context/SpotifyContext.js
+++ src/context/SpotifyContext.js
@@ -1,4 +1,4 @@
-import {createContext, useReducer } from 'react';
+import {createContext, useReducer, useEffect } from 'react';
 
 export const SpotifyContext = createContext();
 
@@ -15,6 +15,11 @@ export const spotifyReducer = (state, action) => {
                 accessToken: action.payload.accessToken,
                 refreshToken: action.payload.refreshToken !== undefined ? action.payload.refreshToken : state.refreshToken,
                 expiresIn: action.payload.expiresIn
+            }
+        case 'SET_SPOTIFY_PLAYING_TRACK':
+            return {
+                ...state,
+                playingTrack: action.payload
             }
         case 'REMOVE_SPOTIFY_TOKEN': 
             return {
@@ -33,18 +38,25 @@ export const SpotifyContextProvider = ( {children} ) => {
         spotifyCode: null,
         accessToken: null,
         refreshToken: null, 
-        expiresIn: null
+        expiresIn: null,
+        playingTrack: null
     })
 
-    // useEffect(()=> {
-    //     const spotifyCode = JSON.parse(localStorage.getItem('spotify-code'))
-    //     if (spotifyCode) {
-    //         dispatch({
-    //             type: 'SET_SPOTIFY_CODE', 
-    //             payload: spotifyCode
-    //         })
-    //     }
-    // }, [])
+    useEffect(()=> {
+        const spotifyCode = JSON.parse(localStorage.getItem('spotifyCode'))
+        if (spotifyCode) {
+            dispatch({
+                type: 'SET_SPOTIFY_CODE', 
+                payload: spotifyCode
+            })
+        }
+    }, [])
+
+    useEffect(() => {
+        if (state.spotifyCode) {
+            localStorage.setItem('spotifyCode', JSON.stringify(state.spotifyCode))
+        }
+    }, [state.spotifyCode])
     
     console.log("SpotifyContext state: ", state)
 
