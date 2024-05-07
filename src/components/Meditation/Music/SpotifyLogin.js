0f4d16ee2b43f200c5976e0e53361418d3cb47fc
Hennessy Nguyen <anguyen137@fordham.edu>
2024-05-07 13:41:35 -0400

    minor change - unimportant

--- src/components/Meditation/Music/SpotifyLogin.js
+++ src/components/Meditation/Music/SpotifyLogin.js
@@ -3,11 +3,11 @@ import { Button } from "@chakra-ui/react";
 
 
 const SpotifyLogin = () => {
-    // const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=4689c7fc29174c6d9523aca2473efe45&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
     const baseURL = "https://accounts.spotify.com/authorize";
     const clientId = "4689c7fc29174c6d9523aca2473efe45";
     const responseType = "code";
     const redirectURI = "http://localhost:3000";
+    // const redirectURI = "http://localhost:3000/meditation"
     const scopes = [
         "streaming",
         "user-read-email",
@@ -17,6 +17,8 @@ const SpotifyLogin = () => {
         "user-read-playback-state",
         "user-modify-playback-state"
     ];
+
+    // https://accounts.spotify.com/en/login?continue=https%3A%2F%2Faccounts.spotify.com%2Fauthorize%3Fscope%3Dstreaming%2Buser-read-email%2Buser-read-private%2Buser-library-read%2Buser-library-modify%2Buser-read-playback-state%2Buser-modify-playback-state%26response_type%3Dcode%26redirect_uri%3Dhttp%253A%252F%252Flocalhost%253A3000%252Fmeditation%26client_id%3D4689c7fc29174c6d9523aca2473efe45
     
     const AUTH_URL = `${baseURL}?client_id=${clientId}&response_type=${responseType}&redirect_uri=${redirectURI}&scope=${encodeURIComponent(scopes.join(' '))}`;
     
