d8d122f876dc6bea7ec66d05f792b6a90bef63e7
Hennessy Nguyen <anguyen137@fordham.edu>
2024-05-07 14:10:02 -0400

    HomePage [in progress]: should update the user data when get the code

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,29 @@
 import Home from '../components/Home/Home.js';
 import { useSpotifyContext } from '../hooks/useSpotifyContext'
+import { useAuthContext } from '../hooks/useAuthContext'
 import { useEffect } from'react';
+import axios from 'axios';
 
 const HomePage = () => {
-    // const code = new URLSearchParams(window.location.search).get('code');
-    const {dispatch} = useSpotifyContext();
-    // if (code) {
-    //     // localStorage.setItem('spotify-code', JSON.stringify(code));
-    //     dispatch({type: 'SET_SPOTIFY_CODE', payload: code});
-    // }
+    const { dispatch } = useSpotifyContext();
+    const { user } = useAuthContext()
 
     useEffect(() => {
         const code = new URLSearchParams(window.location.search).get('code');
+        console.log('user object before set spotify code: ', user);
         if (code) {
-            // localStorage.setItem('spotify-code', JSON.stringify(code));
             dispatch({type: 'SET_SPOTIFY_CODE', payload: code});
+            console.log('user object after set spotify code: ', user);
+            // axios.patch(`http://localhost:4000/api/users/${user.user._id}`, 
+            //     { spotifyCode: code }, 
+            //     // { headers: {Authorization: `Bearer ${user.token}`} }
+            // )
+            //     .then(response => {
+            //         console.log('Update user SpotifyCode successful:', response.data);
+            //     })
+            //     .catch(error => {
+            //         console.error('Error while updating user SpotifyCode', error);
+            //     });
         }
     }, [dispatch]); 
     
