3ee75554d9d774136007279c2a6348989ab3beeb
Hennessy Nguyen <anguyen137@fordham.edu>
2024-05-16 03:33:16 +0700

    create prompt

--- src/pages/NewsFeed.js
+++ src/pages/NewsFeed.js
@@ -2,22 +2,24 @@ import CreatePost from "../components/NewsFeed/CreatePost";
 import AllPosts from "../components/NewsFeed/AllPosts";
 import QuotesAndTipsCards from "../components/NewsFeed/QuotesAndTipsCards";
 import {Grid, GridItem } from "@chakra-ui/react"
+import Prompts from "../components/NewsFeed/Prompts/Prompts";
 
 const NewsFeed = () => {
     return (
         <div className="newsfeed">
             <h1>NewsFeed</h1>
 
-            <Grid gridTemplateColumns={'1fr 30%'} m={10} gap={6}>
+            <Prompts/>
+
+            {/* <Grid gridTemplateColumns={'1fr 30%'} m={10} gap={6}>
                 <GridItem w='100%' h='10'> 
                   <CreatePost/>
-                  {/* <PromptPost /> */}
                   <AllPosts/>
                 </GridItem>
                 <GridItem w='100%' >
                   <QuotesAndTipsCards />
                 </GridItem>
-            </Grid>
+            </Grid> */}
         </div>
     );
 }
