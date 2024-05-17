179b38592d01e9044af9bb60c2b171060861ce94
Nam Nguyen <nguy1053@msu.edu>
2024-05-17 15:00:18 +0700

    Add toggle favorite action type in AudioContext to handle favorite voices

--- src/context/AudioContext.js
+++ src/context/AudioContext.js
@@ -1,40 +1,63 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 
-export const AudioContext = createContext()
+export const AudioContext = createContext();
 
 export const audioReducer = (state, action) => {
-    switch (action.type) {
-        case 'GET_AUDIOS':
-            return {
-                ...state,
-                audios: action.payload
-            }
-        case 'CHOOSE_AUDIO':
-            return {
-                ...state,
-                chosenAudio: action.payload
-            }
-        case 'ADD_AUDIO':
-            return {
-                ...state,
-                audios: [action.payload, ...state.audios]
-            }
-        default: 
-            return state
-    }
-}
+  switch (action.type) {
+    case "GET_AUDIOS":
+      return {
+        ...state,
+        audios: action.payload,
+      };
+    case "CHOOSE_AUDIO":
+      return {
+        ...state,
+        chosenAudio: action.payload,
+      };
+    case "ADD_AUDIO":
+      return {
+        ...state,
+        audios: [action.payload, ...state.audios],
+      };
+    case "TOGGLE_FAVORITE":
+      return {
+        ...state,
+        audios: state.audios.map((audio, _) => {
+          if (audio === action.payload) audio.isFavorite = !audio.isFavorite;
+          return audio;
+        }),
+      };
+    default:
+      return state;
+  }
+};
 
-export const AudioContextProvider = ( { children }) => {
-    const [ state, dispatch ] = useReducer(audioReducer, {
-        audios: [],
-        chosenAudio: null
-    })
+export const AudioContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(audioReducer, {
+    audios: [],
+    chosenAudio: null,
+  });
 
-    /// TODO: useEffect here to get current voices attached with this user
+  /// Dummy data
+  const voices = [
+    {
+      title: "Heartbreak",
+      isFavorite: false,
+    },
+    {
+      title: "Happy day",
+      isFavorite: true,
+    },
+  ];
 
-    return (
-        <AudioContext.Provider value={ {...state, dispatch }}>
-            {children}
-        </AudioContext.Provider>
-    )
-}
+  useEffect(() => {
+    /// TODO: fetch from DB
+    dispatch({ type: "GET_AUDIOS", payload: voices });
+  }, [dispatch]);
+
+  return (
+    <AudioContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AudioContext.Provider>
+  );
+};
