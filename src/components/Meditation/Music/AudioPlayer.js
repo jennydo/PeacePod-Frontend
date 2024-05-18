d97b997cf89b620033e1f75462ec1523e19a77b3
Ethan Nguyen <gsthanhnguyen@gmail.com>
2024-05-17 18:07:13 -0700

    Able to play the most recent audio from Cloudinary

--- src/components/Meditation/Music/AudioPlayer.js
+++ src/components/Meditation/Music/AudioPlayer.js
@@ -1,21 +1,48 @@
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
-import React, { useState } from 'react';
 import './AudioPlayer.css';
-
+import axios from 'axios';
+import { useEffect, useState, useContext } from 'react';
+import { Button, ButtonGroup, Stack, HStack, VStack, StackDivider, Flex } from '@chakra-ui/react'
 
 const Player = () => {
+    const [audioUrl, setAudioUrl] = useState('');
+
+    const getAudio = async () => {
+        try {
+            const response = await axios.get('http://localhost:4000/api/cloudinary/audios/most-recent');
+            console.log("Response from get audio", response.data)
+            setAudioUrl(response.data);
+        } catch (error) {
+            console.error('Error getting audio:', error);
+        }
+    }
+
+    useEffect(() => {
+        getAudio();
+    }, [audioUrl]);
+
     return (
-        <AudioPlayer className='audio-player'
-            src="https://res.cloudinary.com/dufirricm/video/upload/v1715977114/Audios/Sample01.mp3"
-            onPlay={(e) => console.log("onPlay")}
-            autoPlay={false}
-            volume={0.8}
-            progressJumpStep={10000}
-            layout="horizontal"
-            controls
-            header="Now playing: Let it go!"
-        />
+
+        <VStack
+            divider={<StackDivider borderColor='gray.200' />}
+            spacing={4}
+            // align='stretch'
+        >
+            < AudioPlayer className='audio-player'
+                // src="https://res.cloudinary.com/dufirricm/video/upload/v1715991294/PeacePod/Audios/Sample02_vjcfi8.wav"
+                src={audioUrl}
+                onPlay={(e) => console.log("onPlay")}
+                autoPlay={false}
+                volume={0.5}
+                progressJumpStep={10000}
+                layout="horizontal"
+                controls
+                header="Now playing: Let it go!"
+            />
+            <Button colorScheme='blue' onClick={getAudio}>Generate</Button>
+        </VStack>
+
     );
 };
 
