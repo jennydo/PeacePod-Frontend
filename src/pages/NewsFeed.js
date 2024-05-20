091687887137b75f111f70bb727c7142dcfd7a07
Hennessy Nguyen <anguyen137@fordham.edu>
2024-05-20 10:34:47 +0700

    create the fading prompts

--- src/pages/NewsFeed.js
+++ src/pages/NewsFeed.js
@@ -1,23 +1,33 @@
-import CreatePost from "../components/NewsFeed/CreatePost";
-import AllPosts from "../components/NewsFeed/AllPosts";
-import QuotesAndTipsCards from "../components/NewsFeed/QuotesAndTipsCards";
-import {Grid, GridItem } from "@chakra-ui/react"
+// import CreatePost from "../components/NewsFeed/CreatePost";
+// import AllPosts from "../components/NewsFeed/AllPosts";
+// import QuotesAndTipsCards from "../components/NewsFeed/QuotesAndTipsCards";
+import {Grid, GridItem, Text } from "@chakra-ui/react"
+import Prompt from "../components/NewsFeed/Prompts/Prompt";
+import '../components/NewsFeed/Prompts/Prompt.css';
 
 const NewsFeed = () => {
     return (
         <div className="newsfeed">
             <h1>NewsFeed</h1>
+            <div className="wave"></div>
+            <Grid gridTemplateColumns={'25% 1fr'} m={10} gap={6} minHeight="75vh"> 
+              <GridItem w='100%' paddingTop='100px'> 
+                <Prompt/>
+              </GridItem>
+              <GridItem w='100%'>
+                <Text> Postcards go here </Text>
+              </GridItem>
+            </Grid>
 
-            <Grid gridTemplateColumns={'1fr 30%'} m={10} gap={6}>
+            {/* <Grid gridTemplateColumns={'1fr 30%'} m={10} gap={6}>
                 <GridItem w='100%' h='10'> 
                   <CreatePost/>
-                  {/* <PromptPost /> */}
                   <AllPosts/>
                 </GridItem>
                 <GridItem w='100%' >
                   <QuotesAndTipsCards />
                 </GridItem>
-            </Grid>
+            </Grid> */}
         </div>
     );
 }
