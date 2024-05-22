83fa4e4f1eec8afc92fdfe6e5ecbbaddfbe673d3
Hennessy Nguyen <anguyen137@fordham.edu>
2024-05-22 07:23:53 +0700

    get user background from user schema;

--- src/components/Meditation/Background/BackgroundList.js
+++ src/components/Meditation/Background/BackgroundList.js
@@ -2,15 +2,26 @@ import axios from 'axios';
 import { useContext, useEffect } from "react";
 import { CloudinaryContext } from '../../../context/CloudinaryContext';
 import { Grid, GridItem, Box, Image } from "@chakra-ui/react";
+import { useAuthContext } from '../../../hooks/useAuthContext';
 
 const BackgroundList = () => {
     const {images, dispatch, displayedImage} = useContext(CloudinaryContext);
+    const user = useAuthContext();
+    const userId = user.user.user._id;
+    // console.log('from background list', user.user.user._id)
 
     useEffect(() => {
         axios.get('http://localhost:4000/api/cloudinary')
         .then(res => {
             dispatch({ type: 'GET_IMAGES', payload: res.data})
         }).catch(err => console.log(err))
+
+        axios.get(`http://localhost:4000/api/users/findUser/${userId}`)
+        .then(res => {
+            dispatch({ type: 'GET_USER_IMAGES', payload: res.data.uploadedBackgrounds})
+            console.log('')
+        })
+        .catch(err => console.log(err))
     }, [dispatch])
 
     const chooseImage = (image) => {
