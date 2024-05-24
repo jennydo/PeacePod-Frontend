e9ec6e374f694b83e336c5d4ab757fc47a71b20e
Hennessy Nguyen <anguyen137@fordham.edu>
2024-05-24 23:41:04 +0700

    fix the Add response bar

--- src/components/NewsFeed/Prompts/Prompt.js
+++ src/components/NewsFeed/Prompts/Prompt.js
@@ -9,6 +9,7 @@ import {
   InputRightElement,
   Button,
   InputGroup,
+  Spacer
 } from "@chakra-ui/react";
 import { useState, useRef, useEffect } from "react";
 import "./Prompt.css";
@@ -199,7 +200,7 @@ const Prompt = () => {
   };
 
   return (
-    <Box w="100%" h="100%">
+    <Box h="75vh" w="100%">
       <VStack h="100%" alignItems="stretch">
         <HStack alignItems="flex-start">
           <Box w="40%" onClick={handleClickPrompt} textAlign="right">
@@ -226,6 +227,9 @@ const Prompt = () => {
             </Grid>
           </Box>
         </HStack>
+
+        <Spacer />
+
         <Box>
           <InputGroup size="md">
             <Input
