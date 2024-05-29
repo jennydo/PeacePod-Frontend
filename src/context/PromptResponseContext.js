d856f8b320f1a5bb32fcde7dd293d4c297dec8bd
Nam Nguyen <nguy1053@msu.edu>
2024-05-29 16:25:26 +0700

    Create prompt response context

---
+++ src/context/PromptResponseContext.js
@@ -0,0 +1,68 @@
+import axios from "axios";
+import { createContext, useEffect, useReducer } from "react";
+
+export const PromptResponsesContext = createContext();
+
+export const promptResponsesReducer = (state, action) => {
+  switch (action.type) {
+    case "GET_PROMPT_RESPONSES":
+      return {
+        promptResponses: action.payload,
+        firstPromptResponse: action.payload.length ? action.payload[0] : "",
+      };
+    case "CREATE_PROMPT_RESPONSE":
+      return {
+        promptResponses: [
+            action.payload,
+            ...state.promptResponses
+        ],
+        firstPromptResponse: action.payload,
+      };
+    case "CLEAR":
+      return {
+        promptResponses: [],
+        firstPromptResponse: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export const PromptResponsesContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(promptResponsesReducer, {
+    promptResponses: [],
+    firstPromptResponse: "",
+  });
+
+  const fetchPromptResponses = async () => {
+    const prompt = JSON.parse(localStorage.getItem("new-prompt"));
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (!prompt) return;
+
+    try {
+      const responses = await axios.get(
+        `http://localhost:4000/api/promptResponses/prompt/${prompt._id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${user?.token}`,
+          },
+        }
+      );
+
+      console.log("All responses for prompt", responses.data);
+      dispatch({ type: "GET_PROMPT_RESPONSES", payload: responses.data });
+    } catch (error) {
+      console.log("Error while fetching prompt responses", error);
+    }
+  };
+  useEffect(() => {
+    fetchPromptResponses()
+  }, [dispatch]);
+
+  return (
+    <PromptResponsesContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </PromptResponsesContext.Provider>
+  );
+};
