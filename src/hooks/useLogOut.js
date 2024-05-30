5c6a562fc467a24a9be1ac2c314bb23f3e1d6355
Nam Nguyen <nguy1053@msu.edu>
2024-05-30 14:57:45 +0700

    remove unnecessary dispatch

--- src/hooks/useLogOut.js
+++ src/hooks/useLogOut.js
@@ -1,15 +1,18 @@
 import { useContext } from 'react'
 import { useAuthContext } from './useAuthContext'
 import { AudioContext } from '../context/AudioContext'
+import { PromptResponsesContext } from '../context/PromptResponseContext'
 
 export const useLogOut = () => {
     const { dispatch } = useAuthContext()
     const { dispatch: audioDispatch } = useContext(AudioContext)
-    const logOut = () => {
+    const { dispatch: promptResponsesDispatch } = useContext(PromptResponsesContext)
+    const logOut = async () => {
         localStorage.removeItem('user')
         // localStorage.removeItem('spotifyCode')
         dispatch({type: "LOGOUT"})
         audioDispatch({ type: "CLEAR"})
+        promptResponsesDispatch({ type: "CLEAR"})
     }
     return {logOut}
 }
