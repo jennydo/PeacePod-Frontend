20b7dd073a6f340e7c4b081dfa4686038740d113
Jenny <dolientrang@gmail.com>
2024-05-30 23:07:36 -0400

    added posts comments and reactions. minor design change

--- src/components/NewsFeed/Posts/NormalPost.js
+++ src/components/NewsFeed/Posts/NormalPost.js
@@ -7,14 +7,19 @@ import {
   Image,
   GridItem, Grid,
   Spacer,
+  Button
 } from "@chakra-ui/react";
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import PostModal from "./PostModal";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { useAuthContext } from "../../../hooks/useAuthContext";
+import { useCommentsContext } from '../../../hooks/useCommentsContext';
 import envelopeImage from '../../../assets/images/envelope.jpg'; 
 import stampImage from '../../../assets/images/stamp3.png'; 
 import stampLoveImage from '../../../assets/images/stamplove.png';
 import letterImage from '../../../assets/images/letter.png';
+import { FaHeart, FaComment, FaRegHeart } from "react-icons/fa";
 
 const NormalPost = ({ post }) => {
   const { user } = useAuthContext()
@@ -28,9 +33,88 @@ const NormalPost = ({ post }) => {
   const { avatar, username, avatarData } = post.userId;
   const stampBackgroundColor = "#" + avatarData.backgroundColor[0]
 
-  const previewNum = 20
+  const previewNum = 15
   const words = content.split(' ');
   const preview = words.slice(0, previewNum).join(' ');
+
+  const [likes, setLikes] = useState("0"); // count of likes
+  const [reacted, setReacted] = useState(false); // boolean to check if the user has reacted to the post
+  const { comments, dispatch } = useCommentsContext();
+
+
+  // to get the User and the Comments object for the post when the modal is opened and closed and when the component is mounted
+  useEffect(() => {
+    // get the Comments object for the post
+    if (isOpen) {
+      axios
+        .get(`http://localhost:4000/api/comments/post/${post._id}`)
+        .then((response) => {
+          dispatch({
+            type: "GET_COMMENTS",
+            payload: response.data,
+          });
+          console.log("Comments response", response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching comments:", error);
+        });
+    } else {
+      dispatch({
+        // clear comments when the modal is closed to avoid showing the previous comments when opening the modal again
+        type: "CLEAR_COMMENTS",
+      });
+    }
+  }, [post, dispatch, isOpen]);
+
+  // get the count of likes when the component is mounted
+  useEffect(() => {
+      axios.get(`http://localhost:4000/api/reactions/total/${post._id}`, {
+        headers: { Authorization: `Bearer ${user.token}` },
+      })
+        .then((response) => {
+          const likes = response.data;
+          setLikes(likes);
+        })
+        .catch((error) => {
+          console.error("Error fetching likes:", error);
+        });
+    }, [post, reacted]);
+  
+  // check if the user has reacted to the post
+  useEffect(() => {
+      axios.get(`http://localhost:4000/api/reactions/isReacted/${post._id}`, {
+        headers: { Authorization: `Bearer ${user.token}` },
+      })
+        .then((response) => {
+          setReacted(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching reaction:", error);
+        });
+    }, [post]);
+  
+  // handle the like/unlike functionality
+  const handleReact = async () => {
+    if (reacted) {
+      await axios.delete(
+        `http://localhost:4000/api/reactions/${post._id}`,
+        {
+          headers: { Authorization: `Bearer ${user.token}` },
+        }
+      );
+      setReacted(false);
+      console.log("Unreacted");
+    } else {
+      await axios.post(
+        `http://localhost:4000/api/reactions/${post._id}`,
+        {
+          headers: { Authorization: `Bearer ${user.token}` },
+        }
+      );
+      setReacted(true);
+      console.log("Reacted");
+    }
+  };
 
   return (
     <Box
@@ -55,54 +139,83 @@ const NormalPost = ({ post }) => {
       >
         <GridItem w="100%" h="30%" mt={5} mb={2}>
           <Flex>
-          <Box>
-          <Text>From: {username}</Text>
-          <Text>Date: {formattedTimeStamp}</Text>
-          </Box>
-          <Spacer/>
-          <Flex justify="end" position="relative">
-            <Box w="80px" mt={8} position="relative" zIndex={2}>
-              <Image src={stampLoveImage} />
+            <Box>
+              <Text>From: {username}</Text>
+              <Text>Date: {formattedTimeStamp}</Text>
             </Box>
-            <Box
-              bgImage={stampImage}
-              w="80px"
-              h="90px"
-              bgSize="cover"
-              bgPosition="center"
-              bgRepeat="no-repeat"
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              p="5px"
-              position="relative"
-              zIndex={1}
-              ml="-40px"
-            >
-              <Box w="80%" h="100%" bg={stampBackgroundColor} ml="7px" pt={3}>
-                <Image src={avatar} />
+            <Spacer />
+            <Flex justify="end" position="relative">
+              <Box w="80px" mt={8} position="relative" zIndex={2}>
+                <Image src={stampLoveImage} />
               </Box>
-            </Box>
-          </Flex>
+              <Box
+                bgImage={stampImage}
+                w="80px"
+                h="90px"
+                bgSize="cover"
+                bgPosition="center"
+                bgRepeat="no-repeat"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                p="5px"
+                position="relative"
+                zIndex={1}
+                ml="-40px"
+              >
+                <Box w="80%" h="100%" bg={stampBackgroundColor} ml="7px" pt={3}>
+                  <Image src={avatar} />
+                </Box>
+              </Box>
+            </Flex>
           </Flex>
         </GridItem>
 
-        <GridItem w="100%" h="50%" mt={18}>
+        <GridItem w="100%" h="50%" mt={2} mb={2}>
+          <Flex flexDir='column'>
           <Box w="100%" h="100%">
             <Text>Title: {title}</Text>
-            <Text fontStyle="italic" as='cite' mr={2}>{preview}</Text>
+            <Text fontStyle="italic" as="cite" mr={2}>
+              {preview}
+            </Text>
             <Text
               onClick={onOpen}
               color="gray.500"
               fontStyle="italic"
-              as='cite'
+              as="cite"
               _hover={{ color: "blue.500", textDecoration: "underline" }}
             >
               Read more...
             </Text>
           </Box>
-        </GridItem>
 
+          <Box>
+          <Flex
+            flexDir='row'
+            flexWrap="wrap"
+            sx={{
+              "& > button": {
+                minW: "136px",
+              },
+            }}
+          >
+            <Button
+              variant="ghost"
+              onClick={handleReact}
+              leftIcon={reacted ? <FaHeart /> : <FaRegHeart />}
+              _hover={{ color: "blue.500" }}
+            >
+              {likes.count === 0 ? "" : likes.count}
+            </Button>
+            <Spacer/>
+            <Button variant="ghost" onClick={onOpen} leftIcon={<FaComment />}>
+              Comment
+            </Button>
+          </Flex>
+          </Box>
+          </Flex>
+        </GridItem>
+        
       </Grid>
 
       <PostModal
--- src/components/NewsFeed/Posts/PostModal.js
+++ src/components/NewsFeed/Posts/PostModal.js
@@ -1,64 +1,122 @@
+import axios from 'axios'
 import {
     Modal,
     ModalOverlay,
     ModalContent, 
     ModalHeader,
     ModalBody,
+    ModalFooter,
     ModalCloseButton,
     Flex,
     Box,
     Text,
     Avatar,
-    Divider
+    Divider,
+    Input,
+    Button
 } from '@chakra-ui/react'
 import letterImage from '../../../assets/images/letter.png';
+import { useCommentsContext } from '../../../hooks/useCommentsContext';
+import { useAuthContext } from "../../../hooks/useAuthContext";
+import { useState, useEffect } from 'react';
+import Comment from './Comment';
 
 const PostModal = ({ finalRef, isOpen, onClose, post, user, formattedTimeStamp}) => {
-    const { avatar, username } = post.userId;
+  const { avatar, username } = post.userId;
+  console.log(isOpen)
+  console.log(onClose)
 
-    return (
-      <Modal
-        finalFocusRef={finalRef}
-        isOpen={isOpen}
-        onClose={onClose}
-        size="5xl"
-        scrollBehavior="inside"
-        motionPreset="slideInBottom"
+  const [newComment, setNewComment] = useState("");
+  const { comments, dispatch } = useCommentsContext();
+
+  // Id of user currently logged in
+  const { user: commentingUser } = useAuthContext();
+  const { _id: commentingUserId } = commentingUser.user;
+
+
+  const handlePostComment = async () => {
+    if (!newComment.trim()) return; // Avoid posting empty comments
+
+    try {
+      const response = await axios.post(
+        `http://localhost:4000/api/comments/${post._id}`,
+        {
+          userId: commentingUserId,
+          content: newComment,
+        },
+        {
+          headers: { Authorization: `Bearer ${commentingUser.token}` },
+        }
+      );
+      setNewComment(""); // Clear the input field after posting the comment
+      dispatch({
+        type: "CREATE_COMMENT",
+        payload: response.data,
+      });
+    } catch (error) {
+      console.error("Error posting comment:", error);
+    }
+  };
+
+  return (
+    <Modal
+      finalFocusRef={finalRef}
+      isOpen={isOpen}
+      onClose={onClose}
+      size="5xl"
+      scrollBehavior="inside"
+      motionPreset="slideInBottom"
+    >
+      <ModalOverlay />
+      <ModalContent
+        sx={{
+          borderRadius: "30px",
+          paddingLeft: "20px",
+          paddingRight: "20px",
+        }}
+        bgImage={letterImage}
+        bgSize="cover"
+        bgPosition="bottom"
+        bgRepeat="no-repeat"
       >
-        <ModalOverlay />
-        <ModalContent
-          sx={{
-            borderRadius: "30px",
-            paddingLeft: "20px",
-            paddingRight: "20px",
-          }}
-          bgImage={letterImage}
-          bgSize="cover"
-          bgPosition="bottom"
-          bgRepeat="no-repeat"
-        >
-          <ModalHeader>{post && post.title}</ModalHeader>
-          <Flex flex="1" gap="5" alignItems="center" flexWrap="wrap" p={4}>
-            {" "}
-            {/* Added padding here */}
-            <Avatar name={username} src={avatar} />
-            <Box>
-              <Text fontSize="md">{username}</Text>
-              <Text fontSize="xs">{formattedTimeStamp}</Text>
-            </Box>
-          </Flex>
+        <ModalHeader>{post && post.title}</ModalHeader>
+        <Flex flex="1" gap="5" alignItems="center" flexWrap="wrap" p={4}>
+          {" "}
+          {/* Added padding here */}
+          <Avatar name={username} src={avatar} />
+          <Box>
+            <Text fontSize="md">{username}</Text>
+            <Text fontSize="xs">{formattedTimeStamp}</Text>
+          </Box>
+        </Flex>
 
-          <ModalCloseButton />
+        <ModalCloseButton />
 
-          <ModalBody style={{ whiteSpace: "pre-line" }}>
-            {post?.content}
-            <Box padding={7}>
-              <Divider w="100%" borderWidth="1px" margin={0} />
-            </Box>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    );
+        <ModalBody style={{ whiteSpace: "pre-line" }}>
+          {post?.content}
+          <Box padding={7}>
+            <Divider w="100%" borderWidth="1px" margin={0} />
+          </Box>
+          {comments &&
+            comments.map((comment, idx) => (
+              <Comment comment={comment} key={idx} />
+            ))}
+        </ModalBody>
+
+        <ModalFooter>
+          <Input
+            placeholder="Your thought"
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+            mr={2}
+          />
+          <Button colorScheme="teal" size="md" onClick={handlePostComment}>
+            Send
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
 
 export default PostModal
