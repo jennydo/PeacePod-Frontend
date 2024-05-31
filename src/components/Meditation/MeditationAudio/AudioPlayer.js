3029fbf10db59967fdf54f87560b42d1c2e4b75e
Nam Nguyen <nguy1053@msu.edu>
2024-05-31 14:34:42 +0700

    Turn off auto play after src change. Consider isPlayingAudio to know what to play: audio or spotify

--- src/components/Meditation/MeditationAudio/AudioPlayer.js
+++ src/components/Meditation/MeditationAudio/AudioPlayer.js
@@ -1,37 +1,45 @@
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
-import './AudioPlayer.css';
-import axios from 'axios';
-import { useEffect, useState, useContext } from 'react';
-import { Button, VStack, StackDivider } from '@chakra-ui/react'
-import { AudioContext } from '../../../context/AudioContext';
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import "./AudioPlayer.css";
+import axios from "axios";
+import { useEffect, useState, useContext } from "react";
+import { Button, VStack, StackDivider } from "@chakra-ui/react";
+import { AudioContext } from "../../../context/AudioContext";
+import { useSpotifyContext } from "../../../hooks/useSpotifyContext";
 
 const Player = () => {
-    const { chosenAudio } = useContext(AudioContext)
+  const { chosenAudio, isPlayingAudio } = useContext(AudioContext);
+  const { playingTrack } = useSpotifyContext();
 
-    console.log("chosen audio in audio playerrrrr", chosenAudio)
+  console.log(
+    "chosen audio in audio player and spotify track",
+    chosenAudio,
+    playingTrack?.uri,
+    isPlayingAudio
+  );
 
-    return (
 
-        <VStack
-            divider={<StackDivider borderColor='gray.200' />}
-            spacing={4}
-            // align='stretch'
-        >
-            < AudioPlayer className='audio-player'
-                // src="https://res.cloudinary.com/dufirricm/video/upload/v1715991294/PeacePod/Audios/Sample02_vjcfi8.wav"
-                src={chosenAudio?.audio}
-                onPlay={(e) => console.log("onPlay")}
-                autoPlay={false}
-                volume={0.5}
-                progressJumpStep={10000}
-                layout="horizontal"
-                controls
-                header={`Now playing: ${chosenAudio?.title}`}
-            />
-        </VStack>
-
-    );
+  return (
+    <VStack
+      divider={<StackDivider borderColor="gray.200" />}
+      spacing={4}
+      // align='stretch'
+    >
+      <AudioPlayer
+        className="audio-player"
+        // src="https://res.cloudinary.com/dufirricm/video/upload/v1715991294/PeacePod/Audios/Sample02_vjcfi8.wav"
+        src={isPlayingAudio ? chosenAudio?.audio : playingTrack?.uri}
+        onPlay={(e) => console.log("onPlay")}
+        // autoPlay={false}
+        autoPlayAfterSrcChange={false}
+        volume={0.5}
+        progressJumpStep={10000}
+        layout="horizontal"
+        controls
+        header={`Now playing: ${isPlayingAudio ? chosenAudio?.title : playingTrack?.uri}`}
+      />
+    </VStack>
+  );
 };
 
 export default Player;
