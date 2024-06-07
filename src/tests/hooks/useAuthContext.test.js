8746800809ecf4ff7abd02ece5f7fd1492e2bf46
Hennessy Nguyen <anguyen137@fordham.edu>
2024-06-07 14:44:11 +0700

    small change;

--- src/tests/hooks/useAuthContext.test.js
+++ src/tests/hooks/useAuthContext.test.js
@@ -48,13 +48,3 @@ describe('useAuthContext', () => {
         expect(result.current.user).toBeNull();
     });
 });
-
-
-// import { render, screen } from '@testing-library/react';
-// import App from '../App';
-
-// it('renders learn react link', () => {
-//   render(<App />); 
-//   const linkElement = screen.queryByText("");
-//   expect(linkElement).toBeInTheDocument();
-// });
