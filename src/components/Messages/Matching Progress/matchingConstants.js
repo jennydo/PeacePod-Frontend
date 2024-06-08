b2a59a16052a8103fbb796c257f771f96fc33f2a
Nam Nguyen <nguy1053@msu.edu>
2024-06-08 15:15:16 +0700

    Fix matching constants

--- src/components/Messages/Matching Progress/matchingConstants.js
+++ src/components/Messages/Matching Progress/matchingConstants.js
@@ -11,12 +11,10 @@ export const meditationFeelings = [
 
 // What type of meditation do you practice most frequently?
 export const meditationPractices = [
-  "Guided meditation",
-  "Mantra meditation",
-  "Loving-kindness meditation",
-  "Body scan meditation",
-  "Breath-focused meditation",
-  "Loving-kindness meditation",
+  "Guided",
+  "Mantra",
+  "Body scan",
+  "Breath-focused",
   "Yoga nidra",
 ];
 
