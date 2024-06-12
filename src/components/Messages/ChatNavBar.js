00a4f9978825d410b50a993665d01f826035dc5f
Nam Nguyen <nguy1053@msu.edu>
2024-06-12 08:58:20 +0700

    Update matching button text before and after submitting match

--- src/components/Messages/ChatNavBar.js
+++ src/components/Messages/ChatNavBar.js
@@ -10,6 +10,7 @@ import {
   ModalOverlay,
   ModalHeader,
   useDisclosure,
+  ButtonGroup,
 } from "@chakra-ui/react";
 import ChatBox from "./ChatBox";
 import { useChatsContext } from "../../hooks/useChatsContext";
@@ -19,6 +20,12 @@ import MatchingModal from "./MatchingModal";
 const ChatNavBar = () => {
   const { user } = useAuthContext();
   const { chats, dispatch } = useChatsContext();
+
+  const finalRef = useRef(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isWaitingForMatch } = JSON.parse(localStorage.getItem("user"));
+
+  const [waitingForMatch, setWaitingForMatch] = useState(isWaitingForMatch)
 
   useEffect(() => {
     axios
@@ -31,21 +38,41 @@ const ChatNavBar = () => {
       .catch((error) => console.log(error));
   }, []);
 
-  const finalRef = useRef(null);
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const handleCancel = () => {
+    console.log("Cancelling match...");
+    localStorage.setItem(
+      "user",
+      JSON.stringify({ ...user, isWaitingForMatch: false })
+    );
+    setWaitingForMatch(!waitingForMatch)
+  };
 
   return (
     <VStack>
       <Heading>Your Messages</Heading>
-      <Button
-        w={"100%"}
-        leftIcon={<FaPeopleArrows />}
-        bg={"#FFAFCC"}
-        _hover={{ background: "#FFD6FF" }}
-        onClick={onOpen}
-      >
-        Need someone new to talk to?
-      </Button>
+      <ButtonGroup w={"100%"} isAttached>
+        <Button
+          w={"100%"}
+          leftIcon={<FaPeopleArrows />}
+          bg={"#FFAFCC"}
+          _hover={{ background: "#FFD6FF" }}
+          onClick={!isWaitingForMatch ? onOpen : null}
+        >
+          {isWaitingForMatch
+            ? "Waiting till 21:00..."
+            : "Need someone new to talk to?"}
+        </Button>
+
+        {isWaitingForMatch ? (
+          <Button
+            onClick={handleCancel}
+            bg={"#A2D2FF"}
+            _hover={{ background: "#CDB4DB" }}
+          >
+            Cancel
+          </Button>
+        ) : undefined}
+      </ButtonGroup>
 
       <MatchingModal finalRef={finalRef} isOpen={isOpen} onClose={onClose} />
 
