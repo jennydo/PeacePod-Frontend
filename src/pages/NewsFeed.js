0df4f90f0b411c08106b890b8b37854e301ee1b7
Nam Nguyen <nguy1053@msu.edu>
2024-06-17 16:39:11 +1000

    Decrease height of tab in newsfeed

--- src/pages/NewsFeed.js
+++ src/pages/NewsFeed.js
@@ -1,52 +1,69 @@
 // import CreatePost from "../components/NewsFeed/CreatePost";
 import AllPosts from "../components/NewsFeed/Posts/AllPosts";
 // import QuotesAndTipsCards from "../components/NewsFeed/QuotesAndTipsCards";
-import {Grid, GridItem, useDisclosure, IconButton, Icon, Tabs, TabList, TabPanels, Tab, TabPanel, Box  } from "@chakra-ui/react"
+import {
+  Grid,
+  GridItem,
+  useDisclosure,
+  IconButton,
+  Icon,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Box,
+  Text,
+} from "@chakra-ui/react";
 import Prompt from "../components/NewsFeed/Prompts/Prompt";
-import '../components/NewsFeed/Prompts/Prompt.scss';
+import "../components/NewsFeed/Prompts/Prompt.scss";
 import CreatePostcard from "../components/NewsFeed/Posts/CreatePostcard";
 import { BsFillSendPlusFill } from "react-icons/bs";
 
 const NewsFeed = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-    return (
-        <div className="newsfeed">
-            <h1>NewsFeed</h1>
-            <Tabs align='center' isFitted mt={5}>
-              <TabList>
-                <Tab>Catch a Postcard flying in the Sky</Tab>
-                <Tab>The Waterfall of Messages</Tab>
-              </TabList>
+  return (
+    <div className="newsfeed">
+      <h1>NewsFeed</h1>
+      <Tabs align="center" isFitted mt={5}>
+        <TabList>
+          <Tab p={0} h={10}>
+            <Text fontSize={20}>Catch a Postcard flying in the Sky</Text>
+          </Tab>
+          <Tab p={0} h={10}>
+            <Text fontSize={20}>The Waterfall of Messages</Text>
+          </Tab>
+        </TabList>
 
-              <TabPanels>
-                <TabPanel pb={0} pt={3}>
-                <Box position="relative">
-                  <Box position="absolute" bottom="0" right="20%">
-                    <IconButton
-                      onClick={onOpen}
-                      isRound={true}
-                      variant='solid'
-                      bg='transparent'
-                      aria-label='Done'
-                      fontSize='20px'
-                      icon={<Icon as={BsFillSendPlusFill} boxSize={10}/>}
-                    />
-                  </Box>
-                  <Box display="flex" justifyContent="center">
-                    <AllPosts/>
-                  </Box>
-                  <CreatePostcard isOpen={isOpen} onClose={onClose}/>
-                </Box>
-                </TabPanel>
-                
-                <TabPanel  pb={0}>
-                  <Prompt/>
-                </TabPanel>
-              </TabPanels>
-            </Tabs>
-        </div>
-    );
-}
+        <TabPanels>
+          <TabPanel pb={0} pt={3}>
+            <Box position="relative">
+              <Box position="absolute" bottom="0" right="20%">
+                <IconButton
+                  onClick={onOpen}
+                  isRound={true}
+                  variant="solid"
+                  bg="transparent"
+                  aria-label="Done"
+                  fontSize="20px"
+                  icon={<Icon as={BsFillSendPlusFill} boxSize={10} />}
+                />
+              </Box>
+              <Box display="flex" justifyContent="center">
+                <AllPosts />
+              </Box>
+              <CreatePostcard isOpen={isOpen} onClose={onClose} />
+            </Box>
+          </TabPanel>
+
+          <TabPanel pb={0}>
+            <Prompt />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </div>
+  );
+};
 
 export default NewsFeed;
