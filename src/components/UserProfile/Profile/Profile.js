4ec8d9a48f7fe4fd130fbff3cdaf5a802984bc8d
Ethan Nguyen <gsthanhnguyen@gmail.com>
2024-06-20 15:55:25 -0700

    Done: limit the max selection for Interests are 5

--- src/components/UserProfile/Profile/Profile.js
+++ src/components/UserProfile/Profile/Profile.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { 
+import React, { useState } from 'react'
+import {
     Center,
     VStack,
     Circle,
@@ -19,107 +19,146 @@ import { useAvatarContext } from '../../../hooks/useAvatarContext'
 const Profile = () => {
     const { user } = useAuthContext()
     const age = user.age;
-    const { username, gender, interests, location, pronounce, sexualOrientation, bio } = user.user 
+    const { username, gender, interests, location, pronounce, sexualOrientation, bio } = user.user
     const { avatar } = useAvatarContext()
+
+    // const [selectedInterests, setSelectedInterests] = useState([]);
+
+    // const handleSelect = (interest) => {
+    //     if (selectedInterests.includes(interest)) {
+    //         setSelectedInterests(selectedInterests.filter(item => item !== interest));
+    //     } else if (selectedInterests.length < 5) {
+    //         setSelectedInterests([...selectedInterests, interest]);
+    //     }
+    // };
 
     // console.log("avatar", avatar)
 
     return (
-    <VStack>
-        <HStack>
-            <Center h='75px' fontSize='3xl' color='#98B9F2'>
-                <h1>User Profile</h1>
-            </Center>   
-            <EditIcon w={10} h={10} color='aliceblue'/>                
-        </HStack>
-        
-        {avatar && 
-            <Image
-                src={avatar}
-                alt="User Profile"
-                borderRadius='full'
-                objectFit='cover'
+        <VStack>
+            <HStack>
+                <Center h='75px' fontSize='3xl' color='#98B9F2'>
+                    <h1>User Profile</h1>
+                </Center>
+                <EditIcon w={10} h={10} color='aliceblue' />
+            </HStack>
+
+            {avatar &&
+                <Image
+                    src={avatar}
+                    alt="User Profile"
+                    borderRadius='full'
+                    objectFit='cover'
                 />
             }
-        <AvatarModal/>
-        <Text fontSize='2xl' color='#98B9F2' marginBottom={0}>{username} ({age} years old)</Text>
-        <Text fontSize='xl'>{`(${pronounce})`}</Text>
+            <AvatarModal />
+            <Text fontSize='2xl' color='#98B9F2' marginBottom={0}>{username} ({age} years old)</Text>
+            <Text fontSize='xl'>{`(${pronounce})`}</Text>
 
-        {/* Gender */}
-        <Grid className='border-20' templateColumns='1fr 2fr' w='50%' alignItems='center'>
-            <GridItem h='100%'>
-                <Text fontSize='2xl'  color='#98B9F2' margin={0} padding='5px' bg='aliceblue' alignContent={'center'} borderRadius='10px 0px 0px 10px'>
-                    Gender
-                </Text>                    
-            </GridItem>
+            {/* Gender */}
+            <Grid className='border-20' templateColumns='1fr 2fr' w='50%' alignItems='center'>
+                <GridItem h='100%'>
+                    <Text fontSize='2xl' color='#98B9F2' margin={0} padding='5px' bg='aliceblue' alignContent={'center'} borderRadius='10px 0px 0px 10px'>
+                        Gender
+                    </Text>
+                </GridItem>
 
-            <GridItem h='100%'>
-                <Text fontSize='lg' color='black' margin={0} padding='5px' bg='#FFAFCC' h='100%' alignContent={'center'} borderRadius='0px 10px 10px 0px'>
-                    {gender}
-                </Text>                   
-            </GridItem> 
-        </Grid>            
-
-
-        {/* Location */}
-        <Grid className='border-20' templateColumns='1fr 2fr' w='50%' alignItems='center' >
-            <GridItem h='100%'>
-                <Text fontSize='2xl'  color='#98B9F2' margin={0} padding='5px' bg='aliceblue' alignContent={'center'} borderRadius='10px 0px 0px 10px'>
-                    Location
-                </Text>                    
-            </GridItem>
-
-            <GridItem h='100%'>
-                <Text fontSize='lg' color='black' margin={0} padding='5px' bg='#FFAFCC' h='100%' alignContent={'center'} borderRadius='0px 10px 10px 0px'>
-                    {location}
-                </Text>                   
-            </GridItem>
-        </Grid>           
+                <GridItem h='100%'>
+                    <Text fontSize='lg' color='black' margin={0} padding='5px' bg='#FFAFCC' h='100%' alignContent={'center'} borderRadius='0px 10px 10px 0px'>
+                        {gender}
+                    </Text>
+                </GridItem>
+            </Grid>
 
 
-        {/* Sexual Orientation */}
-        <Box w='50%' borderRadius='20px' padding='10px' bg='aliceblue'>
-            <Text fontSize='2xl' align='left' color='#98B9F2'>
-                Sexual Orientation
-            </Text>
-            <HStack>
-            {
-                sexualOrientation.map((sexOr, idx) => (
-                    <Box as='button' key={idx} className="interest" borderRadius='md' bg='pink' color='white' px={4} h={8} minWidth='auto'>
-                        {sexOr}
-                    </Box> 
-                ))
-            }
-            </HStack>
-        </Box>
+            {/* Location */}
+            <Grid className='border-20' templateColumns='1fr 2fr' w='50%' alignItems='center' >
+                <GridItem h='100%'>
+                    <Text fontSize='2xl' color='#98B9F2' margin={0} padding='5px' bg='aliceblue' alignContent={'center'} borderRadius='10px 0px 0px 10px'>
+                        Location
+                    </Text>
+                </GridItem>
 
-        {/* Hobbies */}
-        <Box w='50%' borderRadius='20px' padding='10px' bg='aliceblue'>
-            <Text fontSize='2xl' align='left' color='#98B9F2'>
-                Hobbies
-            </Text>
-            <HStack>
-            {
-                interests.map((interest, idx) => (
-                    <Box as='button' key={idx} className="interest" borderRadius='md' bg='pink' color='white' px={4} h={8} minWidth='auto'>
-                        {interest}
-                    </Box> 
-                ))
-            }
-            </HStack>
+                <GridItem h='100%'>
+                    <Text fontSize='lg' color='black' margin={0} padding='5px' bg='#FFAFCC' h='100%' alignContent={'center'} borderRadius='0px 10px 10px 0px'>
+                        {location}
+                    </Text>
+                </GridItem>
+            </Grid>
 
-        </Box>
-        
-        {/* Bio */}
-        <Box w='50%'borderRadius='20px' padding='10px' bg='aliceblue'>
-            <Text fontSize='2xl' align='left' color='#98B9F2'>
-                About me
-            </Text>     
-            <Text fontSize='lg' color='black'>
-                {bio}
-            </Text>          
-        </Box>
-    </VStack>
+
+            {/* Sexual Orientation */}
+            <Box w='50%' borderRadius='20px' padding='10px' bg='aliceblue'>
+                <Text fontSize='2xl' align='left' color='#98B9F2'>
+                    Sexual Orientation
+                </Text>
+                <HStack>
+                    {
+                        sexualOrientation.map((sexOr, idx) => (
+                            <Box as='button' key={idx} className="interest" borderRadius='md' bg='pink' color='white' px={4} h={8} minWidth='auto'>
+                                {sexOr}
+                            </Box>
+                        ))
+                    }
+                </HStack>
+            </Box>
+
+            {/* Hobbies */}
+            <Box w='50%' borderRadius='20px' padding='10px' bg='aliceblue'>
+                <Text fontSize='2xl' align='left' color='#98B9F2'>
+                    Hobbies
+                </Text>
+                <HStack>
+                    {
+                        interests.map((interest, idx) => (
+                            <Box as='button' key={idx} className="interest" borderRadius='md' bg='pink' color='white' px={4} h={8} minWidth='auto'>
+                                {interest}
+                            </Box>
+                        ))
+                    }
+                </HStack>
+
+            </Box>
+
+            {/* <Box w='50%' borderRadius='20px' padding='10px' bg='aliceblue'>
+                <Text fontSize='2xl' align='left' color='#98B9F2'>
+                    Hobbies
+                </Text>
+                <HStack>
+                    {interests.map((interest, idx) => (
+                        <Box
+                            as='button'
+                            key={idx}
+                            onClick={() => handleSelect(interest)}
+                            className="interest"
+                            borderRadius='md'
+                            bg={selectedInterests.includes(interest) ? 'blue' : 'pink'}
+                            color='white'
+                            px={4}
+                            h={8}
+                            minWidth='auto'
+                            disabled={!selectedInterests.includes(interest) && selectedInterests.length >= 5}
+                            style={{
+                                cursor: (!selectedInterests.includes(interest) && selectedInterests.length >= 5) ? 'not-allowed' : 'pointer',
+                                opacity: (!selectedInterests.includes(interest) && selectedInterests.length >= 5) ? 0.6 : 1,
+                            }}
+                        >
+                            {interest}
+                        </Box>
+                    ))}
+                </HStack>
+            </Box> */}
+
+            {/* Bio */}
+            <Box w='50%' borderRadius='20px' padding='10px' bg='aliceblue'>
+                <Text fontSize='2xl' align='left' color='#98B9F2'>
+                    About me
+                </Text>
+                <Text fontSize='lg' color='black'>
+                    {bio}
+                </Text>
+            </Box>
+        </VStack>
     )
 }
 
--- src/components/UserProfile/SignUp/InterestModal.js
+++ src/components/UserProfile/SignUp/InterestModal.js
@@ -1,69 +1,74 @@
 import React from 'react';
-import { 
-    Button, ButtonGroup, 
-    Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter,
-    Center} from '@chakra-ui/react';
+import {
+  Button, ButtonGroup,
+  Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter,
+  Center
+} from '@chakra-ui/react';
 import { useDisclosure } from '@chakra-ui/react';
 
-function InterestModal({interestList, alreadySelectedInterests, setAlreadySelectedInterests }) {
-    const { isOpen, onOpen, onClose } = useDisclosure()
 
-    const handleInterestSelected = (interest) => {
-        if (alreadySelectedInterests.includes(interest)) {
-            setAlreadySelectedInterests(alreadySelectedInterests.filter(item => item !== interest));
-        } else {
-            setAlreadySelectedInterests([...alreadySelectedInterests, interest]);
-        }
-    };
-  
-    return (
-      <>
-  
-        <Button mt={4} onClick={onOpen}>
-          Select
-        </Button>
-        <Modal 
-            isOpen={isOpen} 
-            onClose={onClose} 
-            blockScrollOnMount={false}
-            motionPreset='slideInBottom'
-            scrollBehavior='inside'
-            size='xl'>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Select your interests</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-                <Center>
-                    <ButtonGroup variant='solid' 
-                                display="flex" 
-                                flexWrap="wrap" 
-                                justifyContent="center"  
-                                alignItems="center"      
-                                >
-                        {interestList && interestList.map((interest, index) => (
-                            <Button
-                                key={index}
-                                className="interest-option"
-                                colorScheme={alreadySelectedInterests.includes(interest) ? "blue" : "gray"}
-                                onClick={() => handleInterestSelected(interest)}
-                                m={1}>
-                                {interest}
-                            </Button>
-                        ))}
-                    </ButtonGroup>
-                </Center>
-            </ModalBody>
-  
-            <ModalFooter>
-              <Button colorScheme='blue' mr={3} mx="auto" onClick={onClose}>
-                Save
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
-    )
-  }
+function InterestModal({ interestList, alreadySelectedInterests, setAlreadySelectedInterests }) {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const handleInterestSelected = (interest) => {
+    if (alreadySelectedInterests.includes(interest)) {
+      setAlreadySelectedInterests(alreadySelectedInterests.filter(item => item !== interest));
+    } else {
+      if (alreadySelectedInterests.length < 5) {
+        setAlreadySelectedInterests([...alreadySelectedInterests, interest]);
+      }
+    }
+  };
+
+  return (
+    <>
+
+      <Button mt={4} onClick={onOpen}>
+        Select
+      </Button>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        blockScrollOnMount={false}
+        motionPreset='slideInBottom'
+        scrollBehavior='inside'
+        size='xl'>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Select the 5 topics that interest you most</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Center>
+              <ButtonGroup variant='solid'
+                display="flex"
+                flexWrap="wrap"
+                justifyContent="center"
+                alignItems="center"
+              >
+                {interestList && interestList.map((interest, index) => (
+                  <Button
+                    key={index}
+                    className="interest-option"
+                    colorScheme={alreadySelectedInterests.includes(interest) ? "blue" : "gray"}
+                    onClick={() => handleInterestSelected(interest)}
+                    m={1}>
+                    {interest}
+                  </Button>
+                ))}
+              </ButtonGroup>
+            </Center>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} mx="auto" onClick={onClose}>
+              Save
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
 
 export default InterestModal
+
