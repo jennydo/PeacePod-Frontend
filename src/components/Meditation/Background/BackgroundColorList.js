c9b6306ad4c90c9ab60ade9f2ee472ff1ddf2273
Nam Nguyen <nguy1053@msu.edu>
2024-06-27 10:39:10 +1000

    Background color list in customizing color

---
+++ src/components/Meditation/Background/BackgroundColorList.js
@@ -0,0 +1,42 @@
+import React, { useContext, useState } from "react";
+import { colors } from "../backgroundColorConstants";
+import { Box, Card, Center, Grid, GridItem } from "@chakra-ui/react";
+import { CloudinaryContext } from "../../../context/CloudinaryContext";
+
+const BackgroundColorList = () => {
+  const [chosenColor, setChosenColor] = useState();
+
+  const { dispatch } = useContext(CloudinaryContext)
+
+  const handleChooseColor = (color) => {
+    setChosenColor(color);
+    dispatch({ type: 'DISPLAY_IMAGE', payload: color})
+  };
+
+  return (
+    <Box h="100%" w={"100%"} overflowY={"auto"} objectFit={true}>
+      <Grid gridTemplateColumns={"50% 50%"} gap={1} maxW={"98%"}>
+        {colors &&
+          colors.map((color, idx) => (
+            <GridItem
+              key={idx}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <Center
+                onClick={() => handleChooseColor(color)}
+                border={color == chosenColor ? "3px solid red" : "none"}
+                bg={color}
+                w={140}
+                h={140}
+                borderRadius={10}
+              ></Center>
+            </GridItem>
+          ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default BackgroundColorList;
